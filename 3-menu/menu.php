5608a48cad63ee5fd1810d53362f34613f95a052
Kevin Medina <kevingenial01@gmail.com>
2020-11-25 02:21:40 -0600

    corregidas rutas y finalizado el get de los platillos desde la DB.

--- 3-menu/menu.php
+++ 3-menu/menu.php
@@ -1,45 +1,63 @@
 <?php
-header("Status: 301 Moved Permanently");
-header("Location: http://localhost:8080/sitioCompleto/administrador.html");
-$nombre = $_POST ['nombre'];
-$categoria = $_POST ['categoria'];
-$tamanio = $_POST ['porcion'];
-$precio = $_POST ['precio'];
-$descripcion = $_POST ['descripcion'];
-$img='img_menu';
+    include("../php/conexion.php");
 
-if(isset($_FILES[$img])){
-    echo 'subido'.$_FILES[$img]['name'];
-    $destino='img/plat/'.$_FILES[$img]['name'];
-    $ruta=$_FILES[$img]['tmp_name'];
-    copy($ruta,$destino);
-}else{
-    echo 'no subido';
-}
+    $sql = "SELECT * FROM platillo";
+    $result = mysqli_query($conexion, $sql);
+
+    $myArray = array();
+    if ($result) {
+
+        while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                $myArray[] = $row;
+        }
+        echo json_encode($myArray);
+    }
+
+    $conexion->close();
+?>
+
+<?php
+    /* header("Status: 301 Moved Permanently");
+    header("Location: http://localhost:8080/sitioCompleto/administrador.html");
+    $nombre = $_POST ['nombre'];
+    $categoria = $_POST ['categoria'];
+    $tamanio = $_POST ['porcion'];
+    $precio = $_POST ['precio'];
+    $descripcion = $_POST ['descripcion'];
+    $img='img_menu';
+
+    if(isset($_FILES[$img])){
+        echo 'subido'.$_FILES[$img]['name'];
+        $destino='img/plat/'.$_FILES[$img]['name'];
+        $ruta=$_FILES[$img]['tmp_name'];
+        copy($ruta,$destino);
+    }else{
+        echo 'no subido';
+    }
 
 
-$datos = array(
-       'nombre' => $nombre,
-       'categoria' => $categoria,
-       'tamanio' => $tamanio,
-       'precio' => $precio,
-       'imagen' => $destino,
-       'descripcion' => $descripcion,
-);
+    $datos = array(
+        'nombre' => $nombre,
+        'categoria' => $categoria,
+        'tamanio' => $tamanio,
+        'precio' => $precio,
+        'imagen' => $destino,
+        'descripcion' => $descripcion,
+    );
 
 
-//creamos JSON
-$json_string = json_encode($datos);
-$file = "json/platillos.json";
-$actual = file_get_contents($file); // toma los datos que estan en el img_menu json
-if(empty($actual)){
-    $actual = "[\n";
-    $actual .= $json_string . "]";
-}else{
-    $actual = str_replace("]","",$actual); // el primer parametro es el que se va a sustituir, el segundo es por el cual se va a sustituir y el tercero es la cadena donde lo va a sustituir
-    $actual .= ",\n" . $json_string . "]";
-}
+    //creamos JSON
+    $json_string = json_encode($datos);
+    $file = "json/platillos.json";
+    $actual = file_get_contents($file); // toma los datos que estan en el img_menu json
+    if(empty($actual)){
+        $actual = "[\n";
+        $actual .= $json_string . "]";
+    }else{
+        $actual = str_replace("]","",$actual); // el primer parametro es el que se va a sustituir, el segundo es por el cual se va a sustituir y el tercero es la cadena donde lo va a sustituir
+        $actual .= ",\n" . $json_string . "]";
+    }
 
-file_put_contents($file,$actual); 
+    file_put_contents($file,$actual);  */
 
 ?>
